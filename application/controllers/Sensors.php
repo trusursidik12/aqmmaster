8e584683381583a53a3cfc1c2ee38832f591e509
warihsuryono <warih_suryono@yahoo.com>
2020-02-20 09:54:38 +0700

    aaa

--- application/controllers/Sensors.php
+++ application/controllers/Sensors.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Sensors extends CI_Controller {
+class sensors extends CI_Controller {
 
 	public function index(){
 		$values = new stdClass;
