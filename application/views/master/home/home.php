512d5fc9405f0eb19a43db028b1861ff8e40095f
warihsuryono <warih_suryono@yahoo.com>
2020-02-23 21:12:54 +0700

    fix bug padnum

--- application/views/master/home/home.php
+++ application/views/master/home/home.php
@@ -147,7 +147,8 @@
 			var remaining_h = Math.floor(remaining / 3600);
 			var remaining_m = Math.floor((remaining - (remaining_h*3600)) / 60);
 			var remaining_s = Math.floor(remaining % 60);
-			$("#remaining").html(padnum(remaining_h) + ":" + padnum(remaining_m) + ":" + padnum(remaining_s));
+			// $("#remaining").html(padnum(remaining_h) + ":" + padnum(remaining_m) + ":" + padnum(remaining_s));
+			$("#remaining").html(pump_last);
 			if((sensor.pump_interval * 60) <= pump_state_time){
 				if($('#pump_state').prop('checked') == true) $("#pump_state").bootstrapToggle('off'); 
 				else if($('#pump_state').prop('checked') == false) $("#pump_state").bootstrapToggle('on');
