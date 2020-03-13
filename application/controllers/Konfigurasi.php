af422f7f9b3094ea7a029adda40d2f3edb8cb2df
warih suryono <warih_suryono@yahoo.com>
2020-03-13 15:48:35 +0700

    aaa

--- application/controllers/Konfigurasi.php
+++ application/controllers/Konfigurasi.php
@@ -6,8 +6,17 @@ class Konfigurasi extends CI_Controller {
 	public function index()
 	{
 		if($this->input->get('savedatetime')){
-			exec("echo admin | sudo -S date +%Y%m%d -s \"".str_replace("-","",$_GET["date"])."\" &");
-			exec("echo admin | sudo -S date +%T -s \"".$_GET["time"]."\" &");
+			echo "echo admin | sudo -S date +%Y%m%d -s \"".str_replace("-","",$_GET["date"])."\" &";
+			exec("echo admin | sudo -S date +%Y%m%d -s \"".str_replace("-","",$_GET["date"])."\" &",$output);
+			echo "<pre>";
+			print_r($output);
+			echo "</pre>";
+			echo "echo admin | sudo -S date +%T -s \"".$_GET["time"]."\" &"
+			exec("echo admin | sudo -S date +%T -s \"".$_GET["time"]."\" &",$output);
+			echo "<pre>";
+			print_r($output);
+			echo "</pre>";
+			
 		}
 		
 		if($this->input->post('simpan')){
