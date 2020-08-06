651df3ef248d2feb0f60f876a2f8e91227d33fd8
warih suryono <warih_suryono@yahoo.com>
2020-08-06 18:18:53 +0700

    check connection

--- application/views/master/theme/theme.php
+++ application/views/master/theme/theme.php
@@ -246,7 +246,7 @@
 
     <script>
 	function cek_is_online(){
-		$.ajax({url: "http://103.247.11.149/server_side/api/is_connect.php", success: function(result){
+		$.ajax({url: "https://ispumaps.id/server_side/api/is_connect.php", success: function(result){
 			if(result == "1"){
 				$("#online_indicator").html("<h6 style='color:#00FF00;'>Internet Connected</h6>");
 			} else {
